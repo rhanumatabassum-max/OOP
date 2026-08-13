1cf1d2ab2658479471f1702bd9bee59d1f9e6523
rhanumatabassum-max <rhanumatabassum@gmail.com>
2026-08-13 11:58:01 +0600

    Create Problem15.java

---
+++ Problem15.java
@@ -0,0 +1,15 @@
+public class SwapVariables {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        // Swapping logic
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
